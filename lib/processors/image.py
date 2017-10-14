d51344ee6122779612f6ab6a41a35834dfc96b4a
Mark Madere <webyaml@markmadere.com>
2017-10-13 20:06:40 -0500

    image resizer

--- lib/processors/image.py
+++ lib/processors/image.py
@@ -49,6 +49,140 @@ import classes.processor
 '''
 class Resize(classes.processor.Processor):
 
+	def run(self):
+
+		'''
+		Description:
+			
+			Resize an image
+		'''
+		
+		'''
+		from the docs at https://pypi.python.org/pypi/python-resize-image/1.1.3
+		
+		* `resize_crop` crop the image with a centered rectangle of the specified size.
+		* `resize_cover` resize the image the fill the specified area, crop as needed (same behavior as `background-size: cover`).
+		* `resize_contain` resize the image to can fit in the specified area, keeping the ratio and without crop (same behavior as `background-size: contain`). 
+		* `resize_height` resize the image to the specified height ajusting width to keep the ratio the same.
+		* `resize_width` resize the image to the specified width ajusting height to keep the ratio the same.
+		* `resize_thumbnail` resize image while keeping the ratio trying its best to match the specified size.			
+		'''		
+
+		print('lib.processrs.image.Resize')
+		
+		conf = self.conf
+		
+		# config checks
+		
+		#source
+		if not conf.get('source'):
+			print('no source given')
+			return False
+
+		if not conf['source'].get('path'):
+			print('no source path given')
+			return False
+		
+		src_path = self.element.fnr(conf['source']['path'])
+		
+		if not os.path.isfile(src_path):
+			print("source not found: '%s'" %src_path)
+			return False
+		
+		# break down path into components
+		src_dir,  src_bn = os.path.split(src_path)
+		src_bn_no_ext, src_ext = os.path.splitext(src_bn)
+		
+		# store
+		self.load_data({'format': 'string', 'store': 'src_path', 'value': src_path})
+		self.load_data({'format': 'string', 'store': 'src_dir', 'value': src_dir})
+		self.load_data({'format': 'string', 'store': 'src_bn', 'value': src_bn})
+		self.load_data({'format': 'string', 'store': 'src_bn_no_ext', 'value': src_bn_no_ext})
+		self.load_data({'format': 'string', 'store': 'src_ext', 'value': src_ext.lower()})	
+
+		# Store src_path as specific key
+		if conf.get('store'):
+			self.load_data({'format': 'string', 'store': conf['store'], 'value': src_path})
+		
+		#destination
+		if not conf.get('destination'):
+			print('no destination given')	
+			return False
+
+		# open the source image file
+		f = open(src_path, 'r')
+		src_img = Image.open(f)		
+		
+		# force destination into a list
+		if isinstance(conf['destination'], dict):
+			conf['destination'] = [conf['destination']]
+		
+		methods = {
+			'crop': resizeimage.resize_crop,
+			'cover': resizeimage.resize_cover,
+			'contain': resizeimage.resize_contain,
+			'height': resizeimage.resize_height,
+			'width': resizeimage.resize_width,
+			'thumbnail': resizeimage.resize_thumbnail,
+		}
+
+		# loop through destinations
+		for destination in conf['destination']:
+			
+			# config checks
+			method = destination.get('method', 'cover')
+			
+			if method not in methods:
+				print("resize method '%s' not available.")
+				return False
+				
+			size = None
+			
+			if method == "height":
+				if not destination.get('height'):
+					print("resize height requires hieght to be defined.")
+					return False
+				size = destination['height']
+				
+			if method == "width":
+				if not destination.get('width'):
+					print("resize width requires width to be defined.")
+					return False
+				size = destination['width']
+
+			if not size:
+				
+				if not destination.get('height') and not destination.get('width'):
+					print("resize requires a hieght and width to be defined.")
+					return False
+					
+				size = [destination['width'], destination['height']]
+			
+			if not destination.get('path'):
+				print("resize requires a path for each destination.")
+				return False
+			
+			# Markup path
+			dst_path = self.element.fnr(destination['path'])
+			
+			# Store path
+			if destination.get('store'):
+				self.load_data({'format': 'string', 'store': destination['store'], 'value': dst_path})
+
+			# resize and save image
+			dst_img = methods[method](src_img, size)
+			dst_img.save(dst_path, dst_img.format, quality=conf.get('quality',80))
+			
+		
+		# close the source image file
+		src_img.close()
+
+		return True
+
+
+
+class ResizeOld(classes.processor.Processor):
+
 	'''
 	Description:
 		
@@ -125,139 +259,6 @@ class Resize(classes.processor.Processor):
 				return False
 		
 		return True	
-
-
-
-
-class Resize2(classes.processor.Processor):
-
-	def run(self):
-
-		'''
-		Description:
-			
-			Resize an image
-		'''
-
-		print('lib.processrs.image.Resize2')
-		
-		conf = self.conf
-		
-		# config checks
-		
-		#source
-		if not conf.get('source'):
-			print('no source given')
-			return False
-
-		if not conf['source'].get('path'):
-			print('no source path given')
-			return False
-		
-		src_path = self.element.fnr(conf['source']['path'])
-		
-		if not os.path.isfile(src_path):
-			print("source not found: '%s'" %src_path)
-			return False
-		
-		# break down path into components
-		src_dir,  src_bn = os.path.split(src_path)
-		src_bn_no_ext, src_ext = os.path.splitext(src_bn)
-		
-		# store
-		self.load_data({'format': 'string', 'store': 'src_path', 'value': src_path})
-		self.load_data({'format': 'string', 'store': 'src_dir', 'value': src_dir})
-		self.load_data({'format': 'string', 'store': 'src_bn', 'value': src_bn})
-		self.load_data({'format': 'string', 'store': 'src_bn_no_ext', 'value': src_bn_no_ext})
-		self.load_data({'format': 'string', 'store': 'src_ext', 'value': src_ext.lower()})	
-		
-		#destination
-		if not conf.get('destination'):
-			print('no destination given')	
-			return False
-
-		# open the source image file
-		f = open(src_path, 'r')
-		src_img = Image.open(f)		
-		
-		# force destination into a list
-		if isinstance(conf['destination'], dict):
-			conf['destination'] = [conf['destination']]
-
-		'''
-		* `resize_crop` crop the image with a centered rectangle of the specified size.
-		* `resize_cover` resize the image the fill the specified area, crop as needed (same behavior as `background-size: cover`).
-		* `resize_contain` resize the image to can fit in the specified area, keeping the ratio and without crop (same behavior as `background-size: contain`). 
-		* `resize_height` resize the image to the specified height ajusting width to keep the ratio the same.
-		* `resize_width` resize the image to the specified width ajusting height to keep the ratio the same.
-		* `resize_thumbnail` resize image while keeping the ratio trying its best to match the specified size.			
-		'''
-		
-		methods = {
-			'crop': resizeimage.resize_crop,
-			'cover': resizeimage.resize_cover,
-			'contain': resizeimage.resize_contain,
-			'height': resizeimage.resize_height,
-			'width': resizeimage.resize_width,
-			'thumbnail': resizeimage.resize_thumbnail,
-		}
-
-		# loop through destinations
-		for destination in conf['destination']:
-			
-			# config checks
-			method = destination.get('method', 'cover')
-			
-			if method not in methods:
-				print("resize method '%s' not available.")
-				return False
-				
-			size = None
-			
-			if method == "height":
-				if not destination.get('height'):
-					print("resize height requires hieght to be defined.")
-					return False
-				size = destination['height']
-				
-			if method == "width":
-				if not destination.get('width'):
-					print("resize width requires width to be defined.")
-					return False
-				size = destination['width']
-
-			if not size:
-				
-				if not destination.get('height') and not destination.get('width'):
-					print("resize requires a hieght and width to be defined.")
-					return False
-					
-				size = [destination['width'], destination['height']]
-			
-			if not destination.get('path'):
-				print("resize requires a path for each destination.")
-				return False
-			
-			# Markup path
-			dst_path = self.element.fnr(destination['path'])
-			
-			# Store path
-			if destination.get('store'):
-				self.load_data({'format': 'string', 'store': destination['store'], 'value': dst_path})
-
-			# resize and save image
-			dst_img = methods[method](src_img, size)
-			dst_img.save(dst_path, dst_img.format, quality=conf.get('quality',80))
-			
-		
-		# close the source image file
-		src_img.close()
-
-		return True
-
-
-
-
 
 
 
