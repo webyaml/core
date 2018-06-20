4bd96b59002514787a31b492769e2ea37a06f802
Mark Madere <webyaml@markmadere.com>
2018-06-20 14:55:19 -0500

    utf-8 support

--- lib/processors/emails.py
+++ lib/processors/emails.py
@@ -99,7 +99,7 @@ class Send(classes.processor.Processor):
 			
 		rcptos = self.content.fnr(self.conf['to']).split(',')
 			
-		msg = MIMEText(self.content.fnr(self.conf['body']))
+		msg = MIMEText(self.content.fnr(self.conf['body']), "plain", "utf-8")
 		msg['Subject'] = self.content.fnr(self.conf['subject'])
 		msg['From'] = self.content.fnr(self.conf['from'])
 		msg['To'] = self.content.fnr(self.conf['to'])
