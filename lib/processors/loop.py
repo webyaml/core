c31782dc49c080e5d1953cad4b264fdae2d24831
Mark Madere <webyaml@markmadere.com>
2018-09-22 16:00:10 -0500

    fixed bug

--- lib/processors/loop.py
+++ lib/processors/loop.py
@@ -92,7 +92,8 @@ class Loop(classes.processor.Processor):
 				print('warning - data is not a list')
 				print(type(self.content.data))
 				print(self.content.data.__repr__())
-				return False
+			
+			return False
 				
 			self.content.data = [self.content.data]
 			
